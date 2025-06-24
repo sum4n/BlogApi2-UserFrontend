6af863ed5c4dc17c73b94cfc7e0d4794f9da92f6
Suman Das <sum4ndas@gmail.com>
2025-06-24 21:04:31 +0530

    Use react-router-dom in main.jsx to create routes and make the '/' route redirect to '/posts'

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Navigate,
+} from "react-router-dom";
+import "./index.css";
+import App from "./App.jsx";
+import Post from "./components/Post.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to={"/posts"} />,
+  },
+  {
+    path: "/posts",
+    element: <App />,
+  },
+  {
+    path: "/posts/:postId",
+    element: <Post />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
