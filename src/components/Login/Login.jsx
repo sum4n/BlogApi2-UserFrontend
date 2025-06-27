8ac4afbc630d35966f773733d5d58317413d5e86
Suman Das <sum4ndas@gmail.com>
2025-06-27 06:31:29 +0530

    Add Login component to handle login

---
+++ src/components/Login/Login.jsx
@@ -0,0 +1,73 @@
+import { useNavigate, useOutletContext } from "react-router-dom";
+import { useState } from "react";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // Get outlet context function to set token
+  const getToken = useOutletContext();
+
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // console.log(password, email);
+
+    fetch("http://localhost:3000/api/auth/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        if (response.message === "Login successful") {
+          localStorage.setItem("jwt-token", response.token);
+          getToken(response.token);
+          setEmail("");
+          setPassword("");
+          // alert("User logged in successfully");
+          // console.log(response);
+          navigate("/posts");
+        } else {
+          alert(response.message);
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (
+    <>
+      <h2>Login:</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="email">Email: </label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="user@email.com"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          required
+        />
+        <label htmlFor="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
