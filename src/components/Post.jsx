4e206accaa371d73b8aa8b08b7f26f2cff84d878
Suman Das <sum4ndas@gmail.com>
2025-06-27 06:26:32 +0530

    Refactor Post.jsx - write better error handling

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,38 +1,37 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import Comments from "./Comments";
-import NavigationBar from "./NavigationBar/NavigationBar";
 
 const Post = () => {
   const [post, setPost] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  // const postId = 5;
   const { postId } = useParams();
-  console.log(postId);
 
   useEffect(() => {
     fetch(`http://localhost:3000/api/posts/${postId}`)
-      .then((response) => {
+      .then(async (response) => {
         if (response.status >= 400) {
-          throw new Error("server error", response.status);
+          // this captures the detailed error messages sent from backend
+          const errorData = await response.json();
+          throw new Error(errorData.error || `server error ${response.status}`);
         }
         return response.json();
       })
-      .then((response) => setPost(response.post))
-      .catch((error) => setError(error))
+      .then((data) => setPost(data.post))
+      .catch((err) => setError(err))
       .finally(() => setLoading(false));
   }, [postId]);
 
   if (loading) return <p>Loading...</p>;
-  if (error) return <p>A network error was encountered</p>;
+  if (error) return <p>{error.message}</p>;
+  if (!post) return <p>No post found</p>;
 
-  console.log(post);
+  // console.log(post);
 
   return (
     <>
-      <NavigationBar />
       <p>{post.title}</p>
       <p>{post.content}</p>
       <Comments postId={postId} />
